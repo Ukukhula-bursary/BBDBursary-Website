84ab06202a5d32aea02be87e69568f882436702d
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-24 15:58:22 +0200

    separate navbar logic

--- index.js
+++ index.js
@@ -8,40 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
   document.getElementById("successMessage").style.display = "none";
-  const hamburgerBar = document.getElementById("hamburger-bar");
-  const hamburgerBarCross = document.getElementById("hamburger-bar-cross");
-  const navMenu = document.getElementById("nav-menu");
-
-  hamburgerBar.classList.add("open");
-  hamburgerBarCross.classList.add("close");
-  navMenu.classList.add("close");
-
-  hamburgerBar.onclick = () => {
-    hamburgerBar.classList.remove("open");
-    hamburgerBar.classList.add("close");
-    hamburgerBarCross.classList.add("open");
-    navMenu.classList.add("open");
-  };
-
-  const navMenuHide = () => {
-    hamburgerBarCross.classList.add("close");
-    hamburgerBarCross.classList.remove("open");
-    hamburgerBar.classList.remove("close");
-    hamburgerBar.classList.add("open");
-    navMenu.classList.remove("open");
-  };
-
-  hamburgerBarCross.onclick = () => {
-    navMenuHide();
-  };
-
-  let navLinks = navMenu.querySelectorAll("a");
-
-  navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
-      navMenuHide();
-    });
-  });
 
   async function submitForm() {
     const currentDateAndTime = new Date();
