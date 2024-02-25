fd76c170cb7b687a75ea92f5b253eb53619bcaf8
mraees93 <raeesdolley76@gmail.com>
2024-02-25 19:00:36 +0200

    added fetch request for getUniverityByIdButton

--- src/js/university.js
+++ src/js/university.js
@@ -30,6 +30,11 @@ const addNewUniversityButton = document.getElementById(
 );
 const newUniversityInput = document.getElementById("new-university");
 
+const getUniverityByIdButton = document.getElementById(
+  "get-univerity-by-id-button"
+);
+const displayUniversityById = document.getElementById("university-by-id");
+
 getApplicationByIdButton.addEventListener("click", (e) => {
   e.preventDefault();
 
@@ -127,5 +132,20 @@ addNewUniversityButton.addEventListener("click", (e) => {
   })
     .then((res) => res.json())
     .then(() => location.reload()) //should get all after posted
+    .catch((err) => console.log(err));
+});
+
+getUniverityByIdButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const url = "/universities/{id}";
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => (displayUniversityById.innerHTML += `${data}`))
     .catch((err) => console.log(err));
 });
