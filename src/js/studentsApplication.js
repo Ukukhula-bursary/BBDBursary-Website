346c83b953c0397c35448a63c327680a3a22d821
mraees93 <raeesdolley76@gmail.com>
2024-02-25 12:19:03 +0200

    added variables of selected html buttons

---
+++ src/js/studentsApplication.js
@@ -0,0 +1,10 @@
+const getStudentByIdButton = document.getElementById("get-student-by-id");
+const getAllStudentApplicationsButton = document.getElementById(
+  "get-student-applications"
+);
+const updateStudentApplicationStatusButton = document.getElementById(
+  "update-student-application-status"
+);
+const updateStudentApplicationColumnValue = document.getElementById(
+  "update-student-application-column-value"
+);
