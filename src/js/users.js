839e73492a3886526ccc8adbaf3147060e7b7be6
mraees93 <raeesdolley76@gmail.com>
2024-02-26 15:30:17 +0200

    debug get user by email

--- src/js/users.js
+++ src/js/users.js
@@ -1,13 +1,15 @@
 const userByEmailButton = document.getElementById("user-by-email-button");
 const displayUserByEmail = document.getElementById("user-by-email");
 
+const userExistsButton = document.getElementById("user-exists-button");
+const displayUserExists = document.getElementById("user-exists");
+
 userByEmailButton.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("clicked");
   const url = "http://localhost:8090/users/get/susan.white@bbd.co.za";
   fetch(url, {
     method: "GET",
-    mode: "no-cors",
+    mode: "cors",
     headers: {
       "Content-Type": "Application/json",
     },
@@ -17,5 +19,25 @@ userByEmailButton.addEventListener("click", (e) => {
       console.log(data);
       displayUserByEmail.innerHTML += `${data}`;
     })
-    .catch((err) => console.log(err, "hi"));
+    .catch((err) => console.log(err));
+});
+
+userExistsButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const url = "http://localhost:8090/users/exists/david.green@bbd.co.za";
+
+  fetch(url, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "Application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      displayUserExists.innerHTML += `${data}`;
+    })
+    .catch((err) => console.log(err));
 });
