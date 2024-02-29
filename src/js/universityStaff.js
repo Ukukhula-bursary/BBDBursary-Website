1ad7687c6f35495ca3404ab785e01d79dfd8263d
mraees93 <raeesdolley76@gmail.com>
2024-02-29 20:43:06 +0200

    added addUniversityStaffButton fetch request

--- src/js/universityStaff.js
+++ src/js/universityStaff.js
@@ -24,3 +24,24 @@ allUniversitiesStaffButton.addEventListener("click", () => {
     .then((data) => console.log(data))
     .catch((err) => console.log(err));
 });
+
+addUniversityStaffButton.addEventListener("click", () => {
+  const url =
+    "https://bursary-api-1709020026838.azurewebsites.net/universitystaff/add";
+
+  fetch(url, {
+    method: "POST",
+    mode: "cors",
+    body: JSON.stringify({
+      firstName: document.getElementById("fname").value,
+      lastName: document.getElementById("lname").value,
+      phoneNumber: document.getElementById("phone-number").value,
+      email: document.getElementById("email").value,
+      universityName: document.getElementById("university-name").value,
+      departmentName: document.getElementById("department-name").value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
