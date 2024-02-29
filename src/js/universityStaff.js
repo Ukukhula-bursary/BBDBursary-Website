9f543123b7ae798c808697233275f13e9b11ccd4
mraees93 <raeesdolley76@gmail.com>
2024-02-29 16:56:14 +0200

    added allUniversityStaffButton variable to get id's

---
+++ src/js/universityStaff.js
@@ -0,0 +1,9 @@
+const allUniversityStaffButton = document.getElementById(
+  "all-university-staff-button"
+);
+const allUniversityStaff = document.getElementById("all-university-staff");
+
+const addUniversityStaffButton = document.getElementById(
+  "add-university-staff-button"
+);
+//const
