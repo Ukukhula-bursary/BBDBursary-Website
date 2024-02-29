f7cfbe9d58afbc04dbd66a9379a8fb127cacbcae
mraees93 <raeesdolley76@gmail.com>
2024-02-29 19:43:57 +0200

    added fetch request for allUniversitiesStaffButton

--- src/js/universityStaff.js
+++ src/js/universityStaff.js
@@ -1,9 +1,26 @@
-const allUniversityStaffButton = document.getElementById(
-  "all-university-staff-button"
+const allUniversitiesStaffButton = document.getElementById(
+  "all-universities-staff-button"
 );
 const allUniversityStaff = document.getElementById("all-university-staff");
 
 const addUniversityStaffButton = document.getElementById(
   "add-university-staff-button"
 );
-//const
+
+const universitySstaffByIdButton = document.getElementById(
+  "university-staff-by-id-button"
+);
+const universityStaffId = document.getElementById("university-staff-by-id");
+
+allUniversitiesStaffButton.addEventListener("click", () => {
+  const url =
+    "https://bursary-api-1709020026838.azurewebsites.net/universitystaff/all";
+
+  fetch(url, {
+    method: "GET",
+    mode: "cors",
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
