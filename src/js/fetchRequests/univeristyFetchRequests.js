865647e0127a1626645abf1c79a629107ba0c6c7
mraees93 <raeesdolley76@gmail.com>
2024-03-01 15:13:07 +0200

    created addNewUniversityButton

--- src/js/fetchRequests/univeristyFetchRequests.js
+++ src/js/fetchRequests/univeristyFetchRequests.js
@@ -1,18 +1,34 @@
+const addNewUniversityButton = document.getElementById(
+  "add-new-university-button"
+);
+
 async function getAllUniversities() {
-    const url =
-        "https://bursary-api-1709020026838.azurewebsites.net/university/all";
-    return fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
+  const url =
+    "https://bursary-api-1709020026838.azurewebsites.net/university/all";
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return data;
     })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data)
-            return data;
-        })
-        .catch((err) => console.log(err));//alert
+    .catch((err) => console.log(err)); //alert
 }
 
-
+addNewUniversityButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const value = newUniversityInput.value;
+  const url = "/universities";
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ UniversityName: value }),
+  })
+    .then((res) => res.json())
+    .then(() => location.reload()) //should get all after posted
+    .catch((err) => console.log(err));
+});
