31a190e6e7a087b624805eafecc7e21592c2f39b
mraees93 <raeesdolley76@gmail.com>
2024-03-02 13:18:26 +0200

    added populateUniversityDropdown function to add universities

--- src/js/universityStaff.js
+++ src/js/universityStaff.js
@@ -11,54 +11,88 @@ const universityStaffByIdButton = document.getElementById(
   "university-staff-by-id-button"
 );
 const universityStaffId = document.getElementById("university-staff-by-id");
-
-allUniversitiesStaffButton.addEventListener("click", () => {
+const universityNameSelect = document.getElementById(
+  "university-name-dropdown"
+);
+async function getAllUniversities() {
   const url =
-    "https://bursary-api-1709020026838.azurewebsites.net/universitystaff/all";
-
-  fetch(url, {
+    "https://bursary-api-1709020026838.azurewebsites.net/university/all";
+  return fetch(url, {
     method: "GET",
-    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
   })
     .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-});
-
-addUniversityStaffButton.addEventListener("click", () => {
-  const url =
-    "https://bursary-api-1709020026838.azurewebsites.net/universitystaff/add";
-
-  fetch(url, {
-    method: "POST",
-    mode: "cors",
-    body: JSON.stringify({
-      firstName: document.getElementById("fname").value,
-      lastName: document.getElementById("lname").value,
-      phoneNumber: document.getElementById("phone-number").value,
-      email: document.getElementById("email").value,
-      universityName: document.getElementById("university-name").value,
-      departmentName: document.getElementById("department-name").value,
-    }),
-  })
-    .then((res) => {
-      res.json();
-      console.log(res.json());
-      //return res.json();
+    .then((data) => {
+      return data;
     })
-    .then((data) => console.log(data))
     .catch((err) => console.log(err));
-});
+}
 
-universityStaffByIdButton.addEventListener("click", () => {
-  const staffId = document.getElementById("staff-id");
-  const url = `https://bursary-api-1709020026838.azurewebsites.net/universitystaff/id=${staffId}`;
+async function populateUniversityDropdown() {
+  universityNameSelect.disabled = true;
+  const universities = await getAllUniversities();
 
-  fetch(url, {
-    method: "GET",
-    mode: "cors",
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-});
+  if (universities.length) {
+    universityNameSelect.disabled = false;
+    for (const university of universities) {
+      const newOption = document.createElement("option");
+      newOption.text = university.universityName;
+      newOption.value = university.universityId;
+      universityNameSelect.add(newOption);
+    }
+  }
+}
+populateUniversityDropdown();
+
+// allUniversitiesStaffButton.addEventListener("click", () => {
+//   const url =
+//     "https://bursary-api-1709020026838.azurewebsites.net/universitystaff/all";
+
+//   fetch(url, {
+//     method: "GET",
+//     mode: "cors",
+//   })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// });
+
+// addUniversityStaffButton.addEventListener("click", () => {
+//   const url =
+//     "https://bursary-api-1709020026838.azurewebsites.net/universitystaff/add";
+
+//   fetch(url, {
+//     method: "POST",
+//     mode: "cors",
+//     body: JSON.stringify({
+//       firstName: document.getElementById("fname").value,
+//       lastName: document.getElementById("lname").value,
+//       phoneNumber: document.getElementById("phone-number").value,
+//       email: document.getElementById("email").value,
+//       universityName: document.getElementById("university-name").value,
+//       departmentName: document.getElementById("department-name").value,
+//     }),
+//   })
+//     .then((res) => {
+//       res.json();
+//       console.log(res.json());
+//       //return res.json();
+//     })
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// });
+
+// universityStaffByIdButton.addEventListener("click", () => {
+//   const staffId = document.getElementById("staff-id");
+//   const url = `https://bursary-api-1709020026838.azurewebsites.net/universitystaff/id=${staffId}`;
+
+//   fetch(url, {
+//     method: "GET",
+//     mode: "cors",
+//   })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// });
