25be81aca7d2fc97f501734b84eaa0a94a1fd85f
mraees93 <raeesdolley76@gmail.com>
2024-03-02 18:33:20 +0200

    added getUniverityByNameButton fetch request

--- src/js/fetchRequests/univeristyFetchRequests.js
+++ src/js/fetchRequests/univeristyFetchRequests.js
@@ -7,6 +7,12 @@ const getUniverityByIdButton = document.getElementById(
 );
 const displayUniversityById = document.getElementById("university-by-id");
 
+const univerityNameInput = document.getElementById("uni-name");
+const getUniverityByNameButton = document.getElementById(
+  "get-univerity-by-name-button"
+);
+const displayUniversityName = document.getElementById("university-by-name");
+
 async function getAllUniversities() {
   const url =
     "https://bursary-api-1709020026838.azurewebsites.net/university/all";
@@ -56,6 +62,23 @@ getUniverityByIdButton.addEventListener("click", () => {
     .then((res) => res.json())
     .then((data) => {
       displayUniversityById.innerHTML += `${data}`;
+      console.log(data);
+    })
+    .catch((err) => console.log(err));
+});
+
+getUniverityByNameButton.addEventListener("click", () => {
+  const universityName = univerityNameInput.value;
+  const url = `https://bursary-api-1709020026838.azurewebsites.net/university/name=${universityName}`;
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      // displayUniversityName.innerHTML += `${data}`;
       console.log(data);
     })
     .catch((err) => console.log(err));
