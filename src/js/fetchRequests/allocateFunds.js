6eca32a13006066b91ebee4f53913519c83e944c
mraees93 <raeesdolley76@gmail.com>
2024-03-03 12:11:16 +0200

    updated html

--- src/js/fetchRequests/allocateFunds.js
+++ src/js/fetchRequests/allocateFunds.js
@@ -3,6 +3,12 @@ const getAllUniversityAllocationsButton = document.getElementById(
   "get-all-uni-allocations"
 );
 
+const yearInput = document.getElementById("year-input");
+const spentByYearSpan = document.getElementById("bbd-yearly-spent-so-far");
+const getAllocationsForYearButton = document.getElementById(
+  "get-allocations-for-year"
+);
+
 getAllUniversityAllocationsButton.addEventListener("click", () => {
   const url =
     "https://bursary-api-1709020026838.azurewebsites.net/university_allocations/all";
@@ -13,7 +19,39 @@ getAllUniversityAllocationsButton.addEventListener("click", () => {
   })
     .then((res) => res.json())
     .then((data) => {
+      console.log(data);
+
       allAllocationsSection.innerHTML += data;
+    })
+    .catch((err) => console.log(err));
+});
+
+getAllocationsForYearButton.addEventListener("click", () => {
+  const year = yearInput.value;
+  const url = `https://bursary-api-1709020026838.azurewebsites.net/university_allocations/year=${year}`;
+
+  fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      spentByYearSpan.innerHTML += data;
+    })
+    .catch((err) => console.log(err));
+});
+
+getAllocationsForYearButton.addEventListener("click", () => {
+  const year = yearInput.value;
+  const url = `https://bursary-api-1709020026838.azurewebsites.net/university_allocations/year=${year}`;
+
+  fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      spentByYearSpan.innerHTML += data;
     })
     .catch((err) => console.log(err));
 });
