3234b368356794fe743c0dc5d6f7da09b457c7f2
TemwaNyirenda <temwanyirenda00@gmail.com>
2024-03-04 23:15:15 +0200

    Completed request access

--- html/hod_view/src/api_request_access.js
+++ html/hod_view/src/api_request_access.js
@@ -1,4 +1,4 @@
-async function populateAndRetrieveApplyStudentActive() {
+async function getStudentApplications() {
     // get all universities
 
     const url = `https://bursary-api-1709020026838.azurewebsites.net/university/all`;
@@ -20,6 +20,8 @@ async function populateAndRetrieveApplyStudentActive() {
     const selectElement = document.getElementById('select-university');
     selectElement.addEventListener('change', (event) => {
 
+        document.getElementById("generic-form").style.display = 'flex';
+
 
         console.log(selectElement.value);
 
@@ -27,9 +29,9 @@ async function populateAndRetrieveApplyStudentActive() {
 
         const section = document.getElementById("select-student");
 
-        // get students from university
+        // generate departments
 
-        const url = `https://bursary-api-1709020026838.azurewebsites.net/students/university_name=${universityName}`;
+        const url = `https://bursary-api-1709020026838.azurewebsites.net/department/all`;
 
         displayLoading()
 
@@ -41,67 +43,42 @@ async function populateAndRetrieveApplyStudentActive() {
             .then((res) => res.json())
             .then((data) => {
                 hideLoading()
-                fillInStudentsList(data)
+                fillInHodDepartmentList(data)
             })
 
             .catch((err) => console.log(err));
 
     });
 
-    // get uni remaining fund
-
-    const url1 = `https://bursary-api-1709020026838.azurewebsites.net/university_allocations/university_name=University of Cape Town/year=2023`;
-
-    displayLoading()
-
-    fetch(url1, {
-        method: "GET",
-        mode: "cors",
-        headers: { "Content-Type": "application/json" },
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            hideLoading()
-            let bursaryAmountInput = document.getElementById("bursary-amount");
-            if (data.amount > 125000) {
-                bursaryAmountInput.max = 125000
-            } else {
-
-                bursaryAmountInput.max = data.amount;
-            }
-        })
-
-        .catch((err) => console.log(err));
-
-    // posting student app
-
-    const selectStudentElement = document.getElementById('select-student');
+    const departmentSelectElement = document.getElementById('hod-department');
 
 
-    const addActiveStudentApplicationButton = document.getElementById(
-        "application_active_student_button"
+    // posting request access app
+
+
+    const requestAccessButton = document.getElementById(
+        "request_access_button"
     );
 
-    addActiveStudentApplicationButton.addEventListener("click", (e) => {
+    requestAccessButton.addEventListener("click", (e) => {
         e.preventDefault();
 
 
         const url =
-            "https://bursary-api-1709020026838.azurewebsites.net/studentapplication/apply/active";
+            "https://bursary-api-1709020026838.azurewebsites.net/universitystaff/add";
         displayLoading()
 
-        console.log("This is the bursary amount")
-        let chosen_bursary_amount = document.getElementById("bursary-amount").value
-        console.log(chosen_bursary_amount)
         fetch(url, {
             method: "POST",
             headers: new Headers({ "Content-Type": "Application/json" }),
             mode: "cors",
             body: JSON.stringify({
-                studentID: selectStudentElement.value,
-                motivation: document.getElementById("motivation").value,
-                bursaryAmount: chosen_bursary_amount,
-                universityStaffID: 1,
+                firstName: document.getElementById("firstName").value,
+                lastName: document.getElementById("lastName").value,
+                phoneNumber: document.getElementById("phone-number").value,
+                email: document.getElementById("emailAddress").value,
+                universityName: selectElement.value,
+                departmentName: departmentSelectElement.value
             }),
         })
             .then((res) => {
@@ -134,17 +111,12 @@ function displayLoading() {
     let loadingGreyedOut = document.getElementById("loading-grey-out");
     loadingGreyedOut.style.display = "block";
 
-    // setTimeout(() => {
-    //     loaderMessage.classList.add("display");
-    //     // loaderMessage.innerText = "Thank you for your patience, we're working hard to display the content for you!";
-    // }, 5000);
 }
 
 function hideLoading() {
     loader.classList.remove("display");
     let loadingGreyedOut = document.getElementById("loading-grey-out");
     loadingGreyedOut.style.display = "none";
-    // loaderMessage.classList.remove("display");
 }
 
 function fillInUniversityList(university_list) {
@@ -163,23 +135,13 @@ function fillInUniversityList(university_list) {
     select.appendChild(el);
 }
 
-function fillInStudentsList(student_list) {
-    let select = document.getElementById("select-student");
+function fillInHodDepartmentList(department_choices) {
+    let select = document.getElementById("hod-department");
 
-    let options = select.getElementsByTagName('option');
 
-    for (let i = options.length; i--;) {
-        select.removeChild(options[i]);
-    }
-
-    let defaultt = document.createElement("option");
-    defaultt.textContent = "Select Student";
-    defaultt.value = "Select Student";
-    defaultt.disabled = true;
-    select.appendChild(defaultt);
-
-    for (let i = 0; i < student_list.length; i++) {
-        let opt = student_list[i].studentId;
+    for (let i = 0; i < department_choices.length; i++) {
+        console.log(i);
+        let opt = department_choices[i].departmentName;
         let el = document.createElement("option");
         el.textContent = opt;
         el.value = opt;
@@ -198,15 +160,6 @@ function successfullySubmit() {
     resultMessageBox.innerText = "Congrats!\nYour application has successfully submitted\nWould you like to create a new  application or go to the dashboard?";
 
     let resultMessageButtons = document.getElementById("resultMessageButtons")
-
-    let createNewApplicationButton = document.createElement("button");
-
-    let createNewApplicationLink = document.createElement("a");
-    createNewApplicationLink.innerText = "New Application";
-    createNewApplicationLink.setAttribute("href", "/html/hod_view/apply_student_active.html");
-
-    createNewApplicationButton.appendChild(createNewApplicationLink);
-    resultMessageButtons.appendChild(createNewApplicationButton);
 
     let goToDashboardButton = document.createElement("button");
 
@@ -230,14 +183,6 @@ function unsuccessfullySubmit() {
 
     let resultMessageButtons = document.getElementById("resultMessageButtons")
 
-    let createNewApplicationButton = document.createElement("button");
-
-    let createNewApplicationLink = document.createElement("a");
-    createNewApplicationLink.innerText = "New Application";
-    createNewApplicationLink.setAttribute("href", "/html/hod_view/apply_student_active.html");
-
-    createNewApplicationButton.appendChild(createNewApplicationLink);
-    resultMessageButtons.appendChild(createNewApplicationButton);
 
     let goToDashboardButton = document.createElement("button");
 
@@ -249,11 +194,11 @@ function unsuccessfullySubmit() {
     resultMessageButtons.appendChild(goToDashboardButton);
 }
 
-
-
 document.addEventListener('DOMContentLoaded', async function () {
-    await populateAndRetrieveApplyStudentActive();
+    await getStudentApplications();
 
 
 
 }, false);
+
+
