1c87c4e06d286cc2a7c3b9b0b546b0c3c189b8be
Sibusiso Mdlovu <53113050+Sibusiso-M@users.noreply.github.com>
2024-03-04 09:37:29 +0200

    prevent spam clicks

--- src/js/fetchRequests/univeristyFetchRequests.js
+++ src/js/fetchRequests/univeristyFetchRequests.js
@@ -11,18 +11,20 @@ async function getAllUniversities() {
   })
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       return data;
     })
-    .catch((err) => console.log(err)); //alert
+    .catch((err) => console.log(err));
 }
-
+document.getElementById("add-new-submit-button").disabled = true;
 addNewUniversityButton.addEventListener("click", (e) => {
   e.preventDefault();
+
   const universityName = document.getElementById("university-name").value;
-  console.log(universityName);
+
   const url =
     "https://bursary-api-1709020026838.azurewebsites.net/university/add";
+  document.getElementById("add-new-submit-button").disabled = true;
+
   fetch(url, {
     method: "POST",
     mode: "cors",
@@ -32,6 +34,12 @@ addNewUniversityButton.addEventListener("click", (e) => {
     }),
   })
     .then((res) => console.log(res.json()))
-    .then((data) => data)
-    .catch((err) => console.log(err));
+    .then((data) => {
+      alert("Institution add successful.");
+      return;
+    })
+    .catch((err) => console.log(err))
+    .finally(() => {
+      document.getElementById("add-new-submit-button").disabled = false;
+    });
 });
