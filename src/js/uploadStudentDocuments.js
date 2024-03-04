4b398656166ed0855d3f40c07a39c195ad0dcd87
mraees93 <raeesdolley76@gmail.com>
2024-03-04 12:39:28 +0200

    debug fetch request

--- src/js/uploadStudentDocuments.js
+++ src/js/uploadStudentDocuments.js
@@ -5,14 +5,17 @@ const uploadCvButton = document.getElementById("upload-cv");
 uploadIdButton.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const file = document.getElementById("myFile").value;
+  const file = document.getElementById("myFile").files[0];
+  const formData = new FormData();
+  formData.append("file", file);
+  //console.log(typeof file, file);
   const url =
     "https://bursary-api-1709020026838.azurewebsites.net/blob/uploadJpg";
   fetch(url, {
     method: "POST",
     mode: "cors",
-    headers: { "Content-Type": "Application/json" },
-    body: JSON.stringify({ file: file }),
+    //headers: { "Content-Type": "Application/json" },
+    body: formData,
   })
     .then((res) => res.json())
     .then((data) => console.log(data))
